407ff11d3b040c48555f1668a66cb7f3e08ca8bc
Michael Sensale <michaelrobert.sensale@gmail.com>
2022-05-24 21:24:49 -0400

    made into a function for scalability

--- tempwebscrape.py
+++ tempwebscrape.py
@@ -1,3 +1,4 @@
+from re import I
 from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
@@ -10,14 +11,12 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.service import Service
 
-#Retrieving the website data
-service = Service('C:\webdrivers\chromedriver.exe')
-driver = webdriver.Chrome(service=service)
-url = 'https://pubchem.ncbi.nlm.nih.gov/compound/106-99-0'
-driver.get(url)
+def scrape(css):
+    service = Service('C:\webdrivers\chromedriver.exe')
+    driver = webdriver.Chrome(service=service)
+    url = 'https://pubchem.ncbi.nlm.nih.gov/compound/106-99-0'
+    driver.get(url)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, css)))
+    return driver.find_element(by=By.CSS_SELECTOR, value=css).text
 
-#finding IUPAC Name
-element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#IUPAC-Name p"))) #
-IUPACName = (driver.find_element(by=By.CSS_SELECTOR, value="#IUPAC-Name p")).text
-
-print(IUPACName)
+IUPACName = scrape("#IUPAC-Name p")
